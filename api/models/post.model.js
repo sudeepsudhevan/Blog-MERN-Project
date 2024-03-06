af81cf06775d0ee22207d00214796a7f05dcbf4d
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-03-06 11:41:25 +0530

    Add create a post api route

---
+++ api/models/post.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        default: "https://images5.alphacoders.com/430/430061.png"
+    },
+    category: {
+        type: String,
+        default: "uncategorized"
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    }
+},
+    {
+        timestamps: true
+    }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
