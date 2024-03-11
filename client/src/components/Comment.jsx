3b511d56b8d9cec7dab30d8a31efa29bea03b342
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-03-11 12:12:04 +0530

    Add delete functionality to the comment section

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -5,7 +5,7 @@ import { FaThumbsUp } from "react-icons/fa";
 import { useSelector } from 'react-redux';
 import { Button, Textarea } from "flowbite-react";
 
-export default function Comment({ comment, onLike, onEdit }) {
+export default function Comment({ comment, onLike, onEdit, onDelete }) {
     const [commentUser, setCommentUser] = useState({});
     const [isEditing, setIsEditing] = useState(false);
     const [editedContent, setEditedContent] = useState(comment.content);
@@ -98,10 +98,16 @@ export default function Comment({ comment, onLike, onEdit }) {
                                 </p>
                                 {
                                     currentUser && (currentUser._id === comment.userId || currentUser.isAdmin) && (
-                                        <button type='button' onClick={handleEdit}
-                                            className="text-gray-400 hover:text-blue-500">
-                                            Edit
-                                        </button>
+                                        <>
+                                            <button type='button' onClick={handleEdit}
+                                                className="text-gray-400 hover:text-blue-500">
+                                                Edit
+                                            </button>
+                                            <button type='button' onClick={() => onDelete(comment._id)}
+                                                className="text-gray-400 hover:text-red-500">
+                                                Delete
+                                            </button>
+                                        </>
                                     )
                                 }
                             </div>
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,15 +1,19 @@
-import { Alert, Button, Textarea } from 'flowbite-react';
+import { Alert, Button, Modal, Textarea } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import Comment from './Comment';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
 
 // eslint-disable-next-line react/prop-types
 export default function CommentSection({ postId }) {
     const { currentUser } = useSelector((state) => state.user)
+    const { theme } = useSelector((state) => state.theme)
     const [comment, setComment] = useState('');
     const [commentError, setCommentError] = useState(null);
     const [comments, setComments] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [commentToDelete, setCommentToDelete] = useState(null);
 
     const navigate = useNavigate();
 
@@ -84,11 +88,30 @@ export default function CommentSection({ postId }) {
         }
     }
 
-    const handelEdit = async (comment, editedComment) => {
+    const handleEdit = async (comment, editedComment) => {
         setComments(
             comments.map((c) => c._id === comment._id ? { ...c, content: editedComment } : c
             )
         );
+    }
+
+    const handleDelete = async (commentId) => {
+        setShowModal(false);
+        try {
+            if (!currentUser) {
+                navigate('/sign-in')
+                return;
+            }
+            const res = await fetch(`/api/comment/deleteComment/${commentId}`, {
+                method: 'DELETE',
+            });
+            if (res.ok) {
+                const data = await res.json();
+                setComments(comments.filter((comment) => comment._id !== commentId))
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
     }
     return (
         <div className='max-w-2xl mx-auto w-full p-3'>
@@ -155,12 +178,42 @@ export default function CommentSection({ postId }) {
                             </div>
                             {
                                 comments.map((comment) => (
-                                    <Comment key={comment._id} comment={comment} onLike={handleLike} onEdit={handelEdit} />
+                                    <Comment key={comment._id} comment={comment} onLike={handleLike}
+                                        onEdit={handleEdit} onDelete={(commentId) => {
+                                            setShowModal(true)
+                                            setCommentToDelete(commentId)
+                                        }} />
                                 ))
                             }
                         </>
                     )
             }
+            <Modal show={showModal} onClose={() => setShowModal(false)} size="md" popup className={theme}>
+                <Modal.Header />
+                <Modal.Body>
+                    <div className="text-center">
+                        <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+                        <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
+                            Are you sure you want to delete this comment?
+                        </h3>
+                    </div>
+
+                    <div className="flex justify-center gap-4">
+                        <Button
+                            color="failure"
+                            onClick={() => handleDelete(commentToDelete)}
+                        >
+                            Yes, I&apos;m sure
+                        </Button>
+                        <Button
+                            color="gray"
+                            onClick={() => setShowModal(false)}
+                        >
+                            No, cancel
+                        </Button>
+                    </div>
+                </Modal.Body>
+            </Modal>
         </div >
     )
 }
